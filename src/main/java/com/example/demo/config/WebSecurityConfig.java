0ca8e05dc98defaa231b014a60a30c6f24a6d16d
jongheonleee <qwefghnm1212@gmail.com>
2025-01-20 19:59:07 +0900

    웹 시큐리티 설정 클래스 추가

---
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(h -> h.defaultSuccessUrl("/main", true)); // 폼 로그인 처리, 로그인 성공시 기본적으로 /main으로 이동
+        http.authorizeHttpRequests(h -> h.anyRequest().authenticated()); // 모든 요청에 대해 인증을 요구
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> {
+            web.ignoring().requestMatchers("/api/v1/**"); // 해당 요청에 대해서는 인증을 무시
+        };
+    }
+}
